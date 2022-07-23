188a7102a00ce1613e11fb512aae8b7712d51dba
“S0me0neR0man” <roman.zitr@yandex.ru>
2022-07-24 01:30:24 +0300

    sprint 2 increment 4 ......

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"github.com/S0me0neR0man/yayaops/internal/client"
+	"time"
 )
 
 // main
 func main() {
-	ctx, _ /*cancel*/ := context.WithCancel(context.Background())
-	_ = client.New().Start(ctx)
-	//time.Sleep(5 * time.Second)
-	//cancel()
-	//c.WaitShutdown()
+	ctx, cancel := context.WithCancel(context.Background())
+	c := client.New().Start(ctx)
+	time.Sleep(10 * time.Minute)
+	cancel()
+	c.WaitShutdown()
 }
